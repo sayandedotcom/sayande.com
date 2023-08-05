d19c011558460fb63620df9b836ab17fd522c339
Sayan De <sayandeten@gmail.com>
2023-08-05 21:18:09 +0530

    small changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <>
       <Header text="Web" />
+      <h1>Yhis Domain is bought by Real Sayan De ! !, sorry !!</h1>
       <Button />
     </>
   );
