df882d69ac07da66320b000e8ece51c10ffaebc0
Sayan De <sayandeten@gmail.com>
2023-09-10 21:10:03 +0530

    second commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,24 @@
-import { Button, Header } from "ui";
+import Footer from "@/components/footer";
+import LeftPanel from "@/components/left-panel";
+import RightPanel from "@/components/right-panel";
 
-export default function Page() {
+export default function Home() {
   return (
-    <>
-      <Header text="Web" />
-      <h1>This Domain is bought by Real Sayan De ! !, sorry !!</h1>
-      <Button />
-    </>
+    <main className="relative flex flex-col items-center flex-1 w-full h-full">
+      {/* Pattern */}
+      <div className="absolute inset-0 z-0 bg-light-pattern dark:bg-dark-pattern bg-verySmall" />
+      {/* Overlay */}
+      <div className="absolute inset-0 z-10 bg-gradient-to-b from-white via-white/90 to-white dark:from-neutral-950 dark:via-neutral-950/90 dark:to-neutral-950" />
+      <div className="container relative z-20 flex flex-col w-full h-full px-6 gap:6 xl:gap-10 xl:flex-row">
+        {/* Left Panel */}
+        <LeftPanel />
+        {/* Right Panel */}
+        <RightPanel />
+        {/* Footer for Mobile */}
+        <div className="flex pb-6 xl:hidden">
+          <Footer />
+        </div>
+      </div>
+    </main>
   );
 }
